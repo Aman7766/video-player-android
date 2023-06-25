8b89af688a49bf6a96602be10a070849cbc5d576
Amandeep <amankumar283@gmail.com>
2023-06-25 03:45:35 -0400

    Local Played

--- app/src/main/java/com/amandeep/videoplayer/MainActivity.java
+++ app/src/main/java/com/amandeep/videoplayer/MainActivity.java
@@ -2,13 +2,23 @@ package com.amandeep.videoplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
+    VideoView vv;
+    MediaPlayer mediaPlayer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        vv=findViewById(R.id.vd);
+
+        String video_path="android.resource://"+getPackageName()+"/"+R.raw.mountains;
+        vv.setVideoPath(video_path);
+        vv.start();
     }
 }
